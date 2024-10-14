7fb955f4b7afdf07876a2618d17c8c385e007763
Deviaripalli08 <deviaripalli88@gmail.com>
2024-10-14 12:29:01 +0530

    Create root.c

---
+++ root.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c,d;
+    float r1,r2;
+    printf("Enter a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    d=(b*b)-(4*a*c);
+    if(d>0)
