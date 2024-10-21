c34d0d64d2412ef9abfe45263bed09cc71983588
Deviaripalli08 <deviaripalli88@gmail.com>
2024-10-21 21:15:12 +0530

    Create ternary.c

---
+++ ternary.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,large;
+    printf("enter a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    large=(a>b&&a>c)?a:(b>c?b:c);
+    printf("largest",large);
+}
