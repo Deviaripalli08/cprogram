6ec3a633374e184c7a87257aa0aa064e9a36be95
Deviaripalli08 <deviaripalli88@gmail.com>
2024-10-21 20:23:55 +0530

    Create vote .c

---
+++ vote .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the a value");
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("eligable");
+    }
+        else
+        {
+            printf("or not");
+            
+        }
+    }
