db5dda3bde694cf5fb3597ee174d6e0301cc020a
Deviaripalli08 <deviaripalli88@gmail.com>
2025-03-12 19:43:38 +0530

    Create season.py

---
+++ season.py
@@ -0,0 +1,9 @@
+month=int(input("enter the month"))
+if month in[1,2,3]:
+ print("spring")
+elif month in[4,5,6] :
+ print("summer")
+elif month in[7,8,9]:
+ print("monsoon")
+elif month in[10,11,12]:
+ print("winter")
